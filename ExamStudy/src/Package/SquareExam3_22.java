e0c90b50b65229970fccbfc38352566bd22fee2f
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:48:42 +0900

    SquareExam3_22 완성

---
+++ ExamStudy/src/Package/SquareExam3_22.java
@@ -0,0 +1,10 @@
+package Package;
+class SquareExam3_22 {
+	public static void main(String[] args) {
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.sqrt(9.0f));
+		System.out.println(Math.sqrt(9.0));
+		System.out.println(Math.sqrt(-9.0));
+	}
+
+}
