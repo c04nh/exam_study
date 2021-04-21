4890874eee2ec1de09dbcd0609073816cd684650
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:45:25 +0900

    RoundExam3_20 완성

---
+++ ExamStudy/src/Package/RoundExam3_20.java
@@ -0,0 +1,12 @@
+package Package;
+class RoundExam3_20 {
+	public static void main(String[] args) {
+		System.out.println(Math.round(25));
+		System.out.println(Math.round(25.3));
+		System.out.println(Math.round(25.5));
+		System.out.println(Math.round(-25.4));
+		System.out.println(Math.round(-25.5));
+		System.out.println(Math.round(-25.6));
+	}
+
+}
