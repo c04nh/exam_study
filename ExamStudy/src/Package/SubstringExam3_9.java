3949e42a109324a34abc026dbbad19a91c8f4b9e
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:10:22 +0900

    SubstringExam3_9

---
+++ ExamStudy/src/Package/SubstringExam3_9.java
@@ -0,0 +1,12 @@
+package Package;
+class SubstringExam3_9 {
+	public static void main(String[] args) {
+		String strData1 = "I love You";
+		String strData3;
+		strData3 = strData1.substring(7);
+		System.out.println("strData3.substring(7) -> " + strData3);
+		strData3 = strData1.substring(2, 5);
+		System.out.println("strData3.substring(2, 5) -> " + strData3);
+	}
+
+}
