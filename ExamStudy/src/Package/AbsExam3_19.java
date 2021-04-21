68375afe48616df4ceb131104f27d9ef4b98cfef
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:44:18 +0900

    AbsExam3_19 완성

---
+++ ExamStudy/src/Package/AbsExam3_19.java
@@ -0,0 +1,10 @@
+package Package;
+class AbsExam3_19 {
+	public static void main(String[] args) {
+		System.out.println(Math.abs(25));
+		System.out.println(Math.abs(-25));
+		System.out.println(Math.abs(25.5));
+		System.out.println(Math.abs(-25.5));
+	}
+
+}
