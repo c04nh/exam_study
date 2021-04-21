ad5cbce686fb1d64b0e69ab73b9eefb420dd87f7
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 22:46:37 +0900

    CharAtExam3_4 완성

---
+++ ExamStudy/src/Package/CharAtExam3_4.java
@@ -0,0 +1,11 @@
+package Package;
+class CharAtExam3_4 {
+	public static void main(String[] args) {
+		String str = "I love Korea";
+		for(int i = 0; i < str.length(); i++) {
+			System.out.print(str.charAt(i));
+		}
+		System.out.println();
+	}
+
+}
