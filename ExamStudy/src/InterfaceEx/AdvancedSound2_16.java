f07323d35eb695af0e0e90e4098387c40ce292f9
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 22:33:08 +0900

    AdvancedSound2_16 완성

---
+++ ExamStudy/src/InterfaceEx/AdvancedSound2_16.java
@@ -0,0 +1,8 @@
+package InterfaceEx;
+interface Sound_1{
+	public void SoundUp(int level);
+	public void SoundDown(int level);
+}
+interface AdvancedSound2_16 extends Sound_1{
+	public void SoundOff();
+}
