7133da16437885e423a1cac1ba096d9e86a49041
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 22:36:06 +0900

    PckageExam3_1 완성

---
+++ ExamStudy/src/Package/PckageExam3_1.java
@@ -0,0 +1,9 @@
+package Package;
+import java.lang.*;
+public class PckageExam3_1 {
+	public static void main(String[] args) {
+		String strData = "I love You";
+		System.out.println("strData = " + strData );
+	}
+
+}
