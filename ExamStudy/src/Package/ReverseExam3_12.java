d9b6ef22ac714a5ccb2e7a08b132f857296450d9
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:22:08 +0900

    ReverseExam3_12 완성

--- ExamStudy/src/Package/ReverseExam3_12.java
+++ ExamStudy/src/Package/ReverseExam3_12.java
@@ -1,10 +1,10 @@
 package Package;
-
-public class ReverseExam3_12 {
-
+class ReverseExam3_12 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringBuffer sb1 = new StringBuffer("java Programming");
+		StringBuffer sb2 = null;
+		sb2 = sb1.reverse();
+		System.out.println("sb2 = " + sb2);
 	}
 
 }
