05a95ba3e02ccc6d043a0ec5a6742c99fc9ab1db
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:20:51 +0900

    ReplaceExam3_11 수정

--- ExamStudy/src/Package/ReplaceExam3_11.java
+++ ExamStudy/src/Package/ReplaceExam3_11.java
@@ -1,7 +1,7 @@
 package Package;
 class ReplaceExam3_11 {
 	public static void main(String[] args) {
-		StringBuffer sb1 = new StringBuffer("java Programing");
+		StringBuffer sb1 = new StringBuffer("java Programming");
 		StringBuffer sb2 = null;
 		sb2 = sb1.replace(0, 4, "JAVA");
 		System.out.println("sb2 = " + sb2);
