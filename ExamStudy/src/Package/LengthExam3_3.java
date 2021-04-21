5cda83f35db2e0db688e6c96aa285a50386ed9a0
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 22:43:39 +0900

    LengthExam3_3 완성

---
+++ ExamStudy/src/Package/LengthExam3_3.java
@@ -0,0 +1,8 @@
+package Package;
+class LengthExam3_3 {
+	public static void main(String[] args) {
+		String strData1 = "I love You";
+		System.out.println("strData1.length()= " + strData1.length());
+	}
+
+}
