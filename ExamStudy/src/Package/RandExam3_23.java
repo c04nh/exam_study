6943190ee4813158dae291489252f52a983ec7a2
nahyun <w2038@e-mirim.hs.kr>
2021-04-21 23:49:39 +0900

    RandExam3_23 완성

---
+++ ExamStudy/src/Package/RandExam3_23.java
@@ -0,0 +1,12 @@
+package Package;
+public class RandExam3_23 {
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println((int)Math.random()* 5);
+		System.out.println((int)(Math.random()* 5));
+		System.out.println((int)(Math.random()* 10));
+		System.out.println((int)(Math.random()*100));
+	}
+
+}
